26649c8be16cb1786eae8136b731e488015ad55b
harriet221 <potter19@dongguk.edu>
2024-01-18 10:16:49 +0900

    start

---
+++ src/main/java/com/ll/edubridge/EduBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.edubridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduBridgeApplication.class, args);
+    }
+
+}
