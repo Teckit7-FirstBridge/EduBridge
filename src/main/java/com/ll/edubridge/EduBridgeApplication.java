4a5f41d1ceaf25172ee12b8908c782ea5bc46280
harriet221 <potter19@dongguk.edu>
2024-01-18 18:34:05 +0900

    setting: @EnableJpaAuditing 추가

--- src/main/java/com/ll/edubridge/EduBridgeApplication.java
+++ src/main/java/com/ll/edubridge/EduBridgeApplication.java
@@ -2,8 +2,10 @@ package com.ll.edubridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EduBridgeApplication {
 
     public static void main(String[] args) {
