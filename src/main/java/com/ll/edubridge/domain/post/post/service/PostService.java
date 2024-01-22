e13079ed59f2a4dfbe66d0cc3cff95db46b4bc63
potato0221 <wnqo10@naver.com>
2024-01-22 10:31:25 +0900

    feat: postService 공통 매서드 구현, fix: rq주소 수정

--- src/main/java/com/ll/edubridge/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/edubridge/domain/post/post/service/PostService.java
@@ -3,7 +3,10 @@ package com.ll.edubridge.domain.post.post.service;
 import com.ll.edubridge.domain.member.member.entity.Member;
 import com.ll.edubridge.domain.post.post.entity.Post;
 import com.ll.edubridge.domain.post.post.repository.PostRepository;
+import com.ll.edubridge.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +16,10 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
+
     private final PostRepository postRepository;
+
+    private final Rq rq;
 
     @Transactional
     public void create(Member author, String title, String content) {
@@ -36,6 +42,62 @@ public class PostService {
                 .build();
 
         postRepository.save(post);
+    }
+
+    @Transactional
+    public void modify(Post post, String title, String content){
+        post.setTitle(title);
+        post.setContent(content);
+        postRepository.save(post);
+    }
+
+    @Transactional
+    public void delete(Post post){
+        postRepository.delete(post);
+    }
+
+    @Transactional
+    public void save(Post post){
+        postRepository.save(post);
+    }
+
+    @Transactional
+    public Page<Post> findAll(Pageable pageable){
+        return postRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public void vote(Post post, Member member){
+        post.getVoter().add(member);
+        postRepository.save(post);
+    }
+
+    @Transactional
+    public void deleteVote(Post post, Member member){
+        post.getVoter().remove(member);
+        postRepository.save(post);
+    }
+
+    public boolean canLike(Member member, Post post){
+        if(member==null) {
+            return false;
+        }
+        return !post.getVoter().contains(member);
+    }
+
+    public boolean canCancelLike(Member member, Post post){
+        if(member==null){
+            return false;
+        }
+        return post.getVoter().contains(member);
+    }
+
+    public boolean haveAuthority(Post post){
+        Member member=rq.getMember();
+
+        if(member==null) return false;
+
+        return post.getWriter().equals(member);
     }
 
     public List<Post> findByPublished(boolean published) {
