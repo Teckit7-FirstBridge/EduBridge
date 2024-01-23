86ba581deab521592858966516e287dceae99e0b
harriet221 <potter19@dongguk.edu>
2024-01-23 10:59:58 +0900

    feat : findByCourseId 자동으로 안 될 걸 대비해 SQL문 추가

--- src/main/java/com/ll/edubridge/domain/course/video/repository/VideoRepository.java
+++ src/main/java/com/ll/edubridge/domain/course/video/repository/VideoRepository.java
@@ -11,4 +11,13 @@ public interface VideoRepository extends JpaRepository<Video, Integer> {
     Video findById(Long id);
 
     List<Video> findByCourseId(Long courseId); // SQL 문 필요할 수 있음
+
+    /*
+    @Query("select "
+            + "distinct v "
+            + "from Video v "
+            + "left outer join Course c1 on c1=v.course "
+            + "where (c1 = :courseId) ")
+    List<Video> findByCourseId(@Param("courseId") Long courseId);
+     */
 }
