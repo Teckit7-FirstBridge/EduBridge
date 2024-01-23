a3b30ef967628b45ece00006f50ede170cdd87df
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-01-23 17:56:20 +0900

    course/1 (#12)
    * docs: [1주차] Project_01팀_진행상황_공유.md
    
    * chore : settings.gradle 플젝 이름 수정 & 중복 Cart.java 삭제
    
    * chore : gitignore에 이슈 템플릿 & 위클리 폴더 추가
    
    * chore : svelte 설치
    
    * docs: [1주차] Project_01팀_진행상황_공유.md 수정
    
    * fix : Rq.java import 패키지 수정
    
    * setting & start
    
    * setting
    
    * feat : video 관련 controller/service/repository/dto 대략적인 얼개 구현
    
    * feat : video controller 매핑 url 수정
    
    * style : @Tag 및 주석 수정
    
    * feature : videoDto에 id 추가
    
    * feat : video 관련 service & repository에서 id 타입 변경 (int → long)
    
    * feat : videoController getResponseBody 수정
    
    * style : 공백 맞춤
    
    * feat : video 관련 강좌/강의 id 타입 변경 (long → Long)
    
    * feat : videoController 관련 responseBody 사용
    
    * feat : video 관련 CRUD 영주님(POST) 코드 참고해서 구현
    
    * feat : videoService getVideo 메서드 @Transactional 추가
    
    * chore : application.yml에서 logging에 프로젝트명, custom pord의 url 수정
    
    * chore : application-prod.yml에서 logging에 프로젝트명 수정
    
    * feat : video 전체 목록 조회 타입 Page -> List 변경
    
    * feat : 특정 video 조회 구현
    
    * chore : videoController 파일명 변경 -> ApiV1VideoController
    
    * refactor : findAll에서 sort의 클래스를 Collections -> List 변경
    
    * feat : findByCourseId 자동으로 안 될 걸 대비해 SQL문 추가
    
    * feat : createVideo 권한 확인 추가 / style : 전체적 파라미터 스타일 수정
    
    * Post/1 (#10) - 프론트 테스트용 merge
    
    * fix: postservice create 기능수정, notprod 수정
    
    * fix: create에서 published를 바꾸지 않도록
    
    * feat: createQna
    
    * feat: postService 공통 매서드 구현, fix: rq주소 수정
    
    * feat: service에서 report 처리
    
    * refactor: postService에서 memberRepo에 직접 접근하지 않고 memberService 이용하도록
    
    * refactor: vote 기능 중 post 찾기를 service에 맡기도록
    
    * fix: admin 권한, post == null 일때 false return하도록 수정
    
    * feat: Page처리 추가
    
    * feat: crud controller에 구현
    
    * feat: 추천 기능 구현
    
    * fix: mapping url 수정
    
    * fix: page오류, pathVariable 오류 수정
    
    * feat: likeByCurrentUser 추가, 현재 사용자가 좋아요 눌렀는지 판단
    
    * feat: qna 구현
    
    * fix: jpa 오류 수정
    
    * feature : video 등록/수정/삭제 권한 수정 - 등록은 권한 없이(차후 로그인 체크 필요), 수정및삭제는 작성자 일치해야 가능
    
    * feature : video 등록/수정/삭제 권한 수정 - 전부 course owner(생성 전)여야 가능하게 수정
    
    * feat : CreatVideoDto 새로 생성
    
    * courseService1
    
    * feat : 강의 CRUD 진행중
    
    * park
    
    * feat : 강좌 CRUD 마무리
    
    ---------
    
    Co-authored-by: Seojeong Yun <101785754+harriet221@users.noreply.github.com>
    Co-authored-by: harriet221 <potter19@dongguk.edu>
    Co-authored-by: potato0221 <wnqo10@naver.com>

--- src/main/java/com/ll/edubridge/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/edubridge/domain/post/post/service/PostService.java
@@ -1,33 +1,171 @@
 package com.ll.edubridge.domain.post.post.service;
 
 import com.ll.edubridge.domain.member.member.entity.Member;
+import com.ll.edubridge.domain.member.member.service.MemberService;
+import com.ll.edubridge.domain.post.post.dto.PostDto;
+import com.ll.edubridge.domain.post.post.dto.QnaDto;
 import com.ll.edubridge.domain.post.post.entity.Post;
 import com.ll.edubridge.domain.post.post.repository.PostRepository;
+import com.ll.edubridge.global.exceptions.GlobalException;
+import com.ll.edubridge.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
+
     private final PostRepository postRepository;
 
+    private final MemberService memberService;
+
+    private final Rq rq;
+
     @Transactional
-    public void write(Member author, String title, String content, boolean published) {
+    public Post create(Member member, PostDto postDto) {
         Post post = Post.builder()
-                // .author(author)
-                .title(title)
-                .content(content)
-                .published(published)
+                .writer(member)
+                .title(postDto.getTitle())
+                .content(postDto.getBody())
                 .build();
 
+        return postRepository.save(post);
+    }
+
+    @Transactional
+    public Post createQna(Member member, QnaDto qnaDto) {
+        Post post = Post.builder()
+                .writer(member)
+                .title(qnaDto.getTitle())
+                .content(qnaDto.getBody())
+                .published(false)
+                .build();
+
+        return postRepository.save(post);
+    }
+
+    @Transactional
+    public Post modify(Long id, PostDto postDto) {
+        Post post=this.getPost(id);
+
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getTitle());
+
+        return postRepository.save(post);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Post post = this.getPost(id);
+        postRepository.delete(post);
+    }
+
+    @Transactional
+    public void save(Post post) {
         postRepository.save(post);
     }
 
-    public List<Post> findByPublished(boolean published) {
-        return postRepository.findByPublishedOrderByIdDesc(published);
+    @Transactional
+    public Page<Post> findAll(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public void vote(Long id, Member member) {
+        Post post = this.getPost(id);
+        post.getVoter().add(member);
+        postRepository.save(post);
+    }
+
+    @Transactional
+    public void deleteVote(Long id, Member member) {
+        Post post = this.getPost(id);
+        post.getVoter().remove(member);
+        postRepository.save(post);
+    }
+
+    public Post getPost(Long id) {
+        Optional<Post> comment = this.findById(id);
+        if (comment.isPresent()) {
+            return comment.get();
+        } else {
+            throw new GlobalException("404-1", "post를 찾을 수 없습니다.");
+        }
+    }
+
+
+    public Optional<Post> findById(Long id) {
+        return postRepository.findById(id);
+    }
+
+//    @Transactional
+//    public void isReported(Post post) {
+//        Member member = post.getWriter();
+//
+//        post.setReport(true);
+//
+//        memberService.isReported(member);
+//
+//        postRepository.save(post);
+//    }
+
+    public Page<Post> getReport(Pageable pageable) {
+        return postRepository.findByReport(pageable, true);
+    }
+
+
+    public boolean canLike(Member member, Post post) {
+        if (member == null) return false;
+        if (post == null) return false;
+
+        return !post.getVoter().contains(member);
+    }
+
+    public boolean canCancelLike(Member member, Post post) {
+        if (member == null) return false;
+        if (post == null) return false;
+
+        return post.getVoter().contains(member);
+    }
+
+    public boolean haveAuthority(Long id) {
+        Member member = rq.getMember();
+
+        Post post = this.getPost(id);
+
+        if (member == null) return false;
+
+        if (rq.isAdmin()) return true;
+
+        return post.getWriter().equals(member);
+    }
+
+    public boolean canRead(Post post){
+        Member member=rq.getMember();
+
+        if(member==null&& post.isPublished()) return true;
+
+        if(rq.isAdmin()) return true;
+
+        if(!post.isPublished()) return false;
+
+        return member.equals(post.getWriter());
+    }
+
+    public Page<Post> findByPublished(boolean published, Pageable pageable) {
+        return postRepository.findByPublishedOrderByIdDesc(published, pageable);
+    }
+
+    public List<Post> getMyQna() {
+        Member member = rq.getMember();
+
+        return postRepository.findByWriterAndPublished(member, false);
     }
 }
