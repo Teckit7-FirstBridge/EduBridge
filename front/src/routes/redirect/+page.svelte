46bb1e0eb6d87c6f5ad81af063b9ab98edffd44a
chanw12 <wooju9812127@gmail.com>
2024-01-25 16:50:44 +0900

    fix: 로그인

---
+++ front/src/routes/redirect/+page.svelte
@@ -0,0 +1,21 @@
+
+<script lang="ts">
+  import { page } from '$app/stores';
+  import rq from '$lib/rq/rq.svelte';
+  import { onMount } from 'svelte';
+  onMount(() => {
+    const provierTypeCode = $page.url.searchParams.get('provierTypeCode');
+    console.log(provierTypeCode);
+    rq.msgInfo('환영합니다.');
+    rq.replace(/);
+  });
+</script>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import rq from '$lib/rq/rq.svelte';
+  onMount(() => {
+    const params = new URLSearchParams(window.location.search);
+    const url = params.get('url');
+    if (url) rq.replace(url);
+  });
+</script>
