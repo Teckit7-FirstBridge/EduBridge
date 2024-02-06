095b49b630702c7a23c8d622ed6a241645baef57
potato0221 <wnqo10@naver.com>
2024-02-06 16:51:41 +0900

    fix: summaryNoteDto size 수정

--- src/main/java/com/ll/edubridge/domain/course/summaryNote/dto/CreateSummaryNoteDto.java
+++ src/main/java/com/ll/edubridge/domain/course/summaryNote/dto/CreateSummaryNoteDto.java
@@ -1,14 +1,12 @@
 package com.ll.edubridge.domain.course.summaryNote.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreateSummaryNoteDto {
 
     @NotEmpty
-    @Size(min=200)
     private String content;
 
     public CreateSummaryNoteDto(String content,Long video_id){
