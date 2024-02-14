2c9591825999c8268c946f223abd65197e01c7fc
potato0221 <wnqo10@naver.com>
2024-02-14 15:56:10 +0900

    fix: 베스트 댓글 좋아요 오류 수정

--- front/src/routes/board/[id]/+page.svelte
+++ front/src/routes/board/[id]/+page.svelte
@@ -455,7 +455,7 @@
                 {/if}
                 {#if rq.member.id == comment.authorId || rq.isAdmin()}
                   <div>
-                    <button class="text-xs" on:click={() => deleteComment(comment.id)}>삭제</button>
+                    <button class="text-xs" on:click={() => deleteComment(comment)}>삭제</button>
                   </div>
                 {/if}
               </div>
@@ -467,7 +467,7 @@
           <div class="flex items-center mr-5">
             <button
               class="btn btn-outline hover:bg-gray-100 hover:text-black flex-col h-14"
-              on:click={() => clickLikedComment(comment.id, comment.likedByCurrentUser)}
+              on:click={() => clickLikedComment(comment)}
             >
               {#if comment.likedByCurrentUser}
                 <svg
